1442ae628b991cf5c19ff08b256a6d43774777f5
Morozov Andrei <102541629+MorozovAdVs@users.noreply.github.com>
2025-01-22 09:14:00 +0300

    Update Add-Schedule.ps1

--- Add-Schedule.ps1
+++ Add-Schedule.ps1
@@ -2,4 +2,4 @@
 #Unregister-ScheduledTask -TaskName "Подключение диска EDS" -Confirm:$false
 
 #Установка задачи монтирования диска A при загрузке компьютера.
-Register-ScheduledTask -Xml (Get-Content “C:\Users\$env:username\Desktop\Подключение диска EDS.xml” | out-string) -TaskName "Подключение диска CERT"
+Register-ScheduledTask -Xml (Get-Content “C:\Подключение диска EDS.xml” | out-string) -TaskName "Подключение диска CERT"
